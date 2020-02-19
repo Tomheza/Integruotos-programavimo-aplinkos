be6728771fc8031bf5bb00ebf1d79b45f51f1fb0
Domantas Rudminas <domis045@gmail.com>
2020-02-19 15:13:21 +0200

    First commit with classes
    First base commit which includes needed classes for v0.1

---
+++ Integruotos-programavimo-aplinkos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Integruotos_programavimo_aplinkos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
