b728b0e495e35e444211b2d139ac05ae6f383915
Domantas Rudminas <domis045@gmail.com>
2020-03-26 21:31:31 +0200

    Documentation 1/3

---
+++ documentation/class_integruotos__programavimo__aplinkos_1_1_grades.js
@@ -0,0 +1,6 @@
+var class_integruotos__programavimo__aplinkos_1_1_grades =
+[
+    [ "exam", "class_integruotos__programavimo__aplinkos_1_1_grades.html#a171af0688ad6b2faa7e687b5e2aaa380", null ],
+    [ "grade", "class_integruotos__programavimo__aplinkos_1_1_grades.html#a4bff52ad8ad1b613b208f60c88cfd0a7", null ],
+    [ "grades", "class_integruotos__programavimo__aplinkos_1_1_grades.html#a4462e789ea7eaf25c4a52c9c7821c15f", null ]
+];
---
+++ documentation/dir_783053fbf8e727b729a19bf3c4b27b89.js
@@ -0,0 +1,9 @@
+var dir_783053fbf8e727b729a19bf3c4b27b89 =
+[
+    [ "Grades.cs", "_grades_8cs.html", [
+      [ "Grades", "class_integruotos__programavimo__aplinkos_1_1_grades.html", "class_integruotos__programavimo__aplinkos_1_1_grades" ]
+    ] ],
+    [ "Student.cs", "_student_8cs.html", [
+      [ "Student", "class_integruotos__programavimo__aplinkos_1_1_student.html", "class_integruotos__programavimo__aplinkos_1_1_student" ]
+    ] ]
+];
