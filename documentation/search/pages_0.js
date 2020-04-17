d5ea648de6f0dae7079ee21ad399f478b1707b59
e0071444 <domantas.rudminas@policija.lt>
2020-04-17 14:34:43 +0300

    # Renew documentation

--- documentation/search/pages_0.js
+++ documentation/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['integruotos_2dprogramavimo_2daplinkos_20v1_2e0_20versija_155',['Integruotos-programavimo-aplinkos v1.0 versija',['../md__c_1__users_e0071444_source_repos_domis045__integruotos-programavimo-aplinkos__r_e_a_d_m_e.html',1,'']]]
+  ['integruotos_2dprogramavimo_2daplinkos_20v1_2e0_20versija_20_2d_20_3ctt_3e1_20strategija_3c_2ftt_3e_152',['Integruotos-programavimo-aplinkos v1.0 versija - &lt;tt&gt;1 strategija&lt;/tt&gt;',['../md__c_1__users_e0071444_source_repos_domis045__integruotos-programavimo-aplinkos__r_e_a_d_m_e.html',1,'']]]
 ];
